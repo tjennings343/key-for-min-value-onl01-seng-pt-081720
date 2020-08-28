dface14c46314f6d78f7de7378a3f9c0e24158cd
Trey Jennings <tjennings343@gmail.com>
2020-08-28 03:29:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,10 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
     if smallest_value == 0 || value < smallest_value
       smallest_value = value
-      lowest_key = key
+      smallest_key = key
     end
   end
-  lowest_key
-
-
+  smallest_key
 end
